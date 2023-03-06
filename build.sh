8f04c36e4dd3b9c51edf6079edcd5da1432581e5
SM7I <sm7i.ham@gmail.com>
2023-03-06 20:43:01 +0100

    Added zip for output

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 docker run --platform linux/amd64 --rm -it -v $PWD:/qdos/QE_driver xora/qdos-devel-a6toa5 /bin/bash -c "cd QE_driver; make"
-
+./zip2-lin -Q4 ethernet.zip dhcp_cde dhcpc_exe qedrv_bin tftp
