e5424f00dc32d7df64347f654ab2757ae8e0b252
SM7I <sm7i.ham@gmail.com>
2023-03-07 10:26:25 +0100

    Detecting OS type

--- build.sh
+++ build.sh
@@ -1,4 +1,27 @@
 #! /bin/sh
 
 docker run --platform linux/amd64 --rm -it -v $PWD:/qdos/QE_driver xora/qdos-devel-a6toa5 /bin/bash -c "cd QE_driver; make"
-./zip2-lin -Q4 ethernet.zip dhcp_cde dhcpc_exe qedrv_bin tftp
+
+# Detect the platform (similar to $OSTYPE)
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    ./zip2-lin -Q4 ethernet.zip dhcp_cde dhcpc_exe qedrv_bin tftp
+    ;;
+  'FreeBSD')
+    OS='FreeBSD'
+    alias ls='ls -G'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin')
+    OS='Mac'
+    ;;
+  'SunOS')
+    OS='Solaris'
+    ;;
+  'AIX') ;;
+  *) ;;
+esac
