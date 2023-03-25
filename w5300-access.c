a292f445b0cd0d2967fda5e24406882c39bef806
Petri Pellinen <petri.pellinen@lekane.com>
2023-03-25 22:42:09 +0200

    w5300-access.c: change all HW reg reads/writes to be 8-bit to support the GoldCard

--- w5300-access.c
+++ w5300-access.c
@@ -12,11 +12,17 @@ void w5300_write_reg8(uint16 reg, uint8 value) {
 }
 
 void w5300_write_reg16(uint16 reg, uint16 value) {
-    *(vuint16 *)(w5300_base_addr + reg) = value;
+    *(vuint8 *)(w5300_base_addr + reg) = (uint8)(value >> 8);
+    *(vuint8 *)(w5300_base_addr + reg + 1) = (uint8)value;
+    /* *(vuint16 *)(w5300_base_addr + reg) = value; */
 }
 
 void w5300_write_reg32(uint16 reg, uint32 value) {
-    *(vuint32 *)(w5300_base_addr + reg) = value;
+    *(vuint8 *)(w5300_base_addr + reg) = (uint8)(value >> 24);
+    *(vuint8 *)(w5300_base_addr + reg + 1) = (uint8)(value >> 16);
+    *(vuint8 *)(w5300_base_addr + reg + 2) = (uint8)(value >> 8);
+    *(vuint8 *)(w5300_base_addr + reg + 3) = (uint8)value;
+    /* *(vuint32 *)(w5300_base_addr + reg) = value; */
 }
 
 void w5300_write_reg_buf(uint16 reg, uint8 *buffer, uint8 size) {
@@ -32,11 +38,21 @@ uint8 w5300_read_reg8(uint16 reg) {
 }
 
 uint16 w5300_read_reg16(uint16 reg) {
-    return (*(vuint16 *)(w5300_base_addr + reg));
+    uint16 result;
+    result = (*(vuint8 *)(w5300_base_addr + reg)) << 8;
+    result += (*(vuint8 *)(w5300_base_addr + reg + 1));
+    return result;
+    /* return (*(vuint16 *)(w5300_base_addr + reg)); */
 }
 
 uint32 w5300_read_reg32(uint32 reg) {
-    return (*(vuint32 *)(w5300_base_addr + reg));
+    uint32 result;
+    result = (*(vuint8 *)(w5300_base_addr + reg)) << 24;
+    result += (*(vuint8 *)(w5300_base_addr + reg + 1)) << 16;
+    result += (*(vuint8 *)(w5300_base_addr + reg + 2)) << 8;
+    result += (*(vuint8 *)(w5300_base_addr + reg + 3));
+    return result;
+    /* return (*(vuint32 *)(w5300_base_addr + reg)); */
 }
 
 void w5300_read_reg_buf(uint16 reg, uint8 *buffer, uint8 size) {
